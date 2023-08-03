d663174f9571ed6141b50f97998ac4d66de5073c
sanjanach10 <83034510+sanjanach10@users.noreply.github.com>
2023-08-03 11:51:11 +0530

    Update Music_Store_Query.sql

--- Music_Store_Query.sql
+++ Music_Store_Query.sql
@@ -1,4 +1,3 @@
-/*	Question Set 1 - Easy */
 
 /* Q1: Who is the senior most employee based on job title? */
 
@@ -46,8 +45,6 @@ LIMIT 1;
 
 
 
-
-/* Question Set 2 - Moderate */
 
 /* Q1: Write query to return the email, first name, last name, & Genre of all Rock Music listeners. 
 Return your list ordered alphabetically by email starting with A. */
@@ -105,7 +102,6 @@ ORDER BY miliseconds DESC;
 
 
 
-/* Question Set 3 - Advance */
 
 /* Q1: Find how much amount spent by each customer on artists? Write a query to return customer name, artist name and total spent */
 
